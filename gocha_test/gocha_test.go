d950e06fa516976c8647c597e91d9213be531bd2
Ichinose Shogo <shogo82148@gmail.com>
2017-05-03 17:38:38 +0900

    add benchmarks for other generators

---
+++ gocha_test/gocha_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/t-mrt/gocha"
+)
+
+func BenchmarkStrRand(b *testing.B) {
+	cases := []struct {
+		name   string
+		regexp string
+	}{
+		{`coffeescript`, `[カコヵか][ッー]{1,3}?[フヒふひ]{1,3}[ィェー]{1,3}[ズス][ドクグュ][リイ][プブぷぶ]{1,3}[トドォ]{1,2}`},
+		{``, `[あ-お]{10}`},
+	}
+
+	for _, c := range cases {
+		_, g := gocha.New(c.regexp)
+		name := c.name
+		if name == "" {
+			name = c.regexp
+		}
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				g.Gen()
+			}
+		})
+	}
+}
